15448cd73291bbbd167e43b7866387f389ff0cee
SiRazoR <33030760+SiRazoR@users.noreply.github.com>
2018-09-07 02:29:16 +0200

    added code to hide browser adress bar on mobile

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ console.log("You can use: ");
 console.log("game.showGameStatus() - will show all important variables");
 console.log("game.resetGame() - will reset board");
 console.log("game.gameWon() - will move to the next level");
+
+// To hide browser address bar on mobile
+window.addEventListener("load",function() {
+    setTimeout(function(){
+        window.scrollTo(0, 1);
+    }, 0);
+});
